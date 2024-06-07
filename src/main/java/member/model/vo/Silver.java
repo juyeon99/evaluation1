c0a6c33c956dad6d27a1f7e94effbda10be52869
juyeon99 <beive999@naver.com>
2024-06-07 17:00:48 +0900

    Implement calculateInterest() abstract method

--- src/main/java/member/model/vo/Silver.java
+++ src/main/java/member/model/vo/Silver.java
@@ -1,9 +1,18 @@
 package member.model.vo;
 
-public class Silver implements Buyable{
+public class Silver extends Member implements Buyable {
+
+    public Silver(String name, String grade, int point) {
+        super(name, grade, point);
+    }
 
     @Override
     public int buy(int price) {
         return price;
+    }
+
+    @Override
+    public double calculateInterest() {
+        return super.getPoint() * 0.02;
     }
 }
--- src/main/java/member/model/vo/Vip.java
+++ src/main/java/member/model/vo/Vip.java
@@ -1,9 +1,18 @@
 package member.model.vo;
 
-public class Vip implements Buyable{
+public class Vip extends Member implements Buyable{
+
+    public Vip(String name, String grade, int point) {
+        super(name, grade, point);
+    }
 
     @Override
     public int buy(int price) {
         return price;
+    }
+
+    @Override
+    public double calculateInterest() {
+        return super.getPoint() * 0.1;
     }
 }
